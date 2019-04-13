1b4b3b99b3b09a9fedcd2a1185669ce5b12e2122
Hoang Anh Ngo <tonyngo.zf@gmail.com>
2019-04-13 23:17:34 +0200

    Update settings.py

--- varwish/varwish/settings.py
+++ varwish/varwish/settings.py
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 REST_FRAMEWORK = {
 	# Use Django's standard `django.contrib.auth` permissions,
