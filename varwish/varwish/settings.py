851fdd2b7c90da062fa50b612818c9f66ff41d48
tonyskansf <tonyngo.zf@gmail.com>
2019-04-14 20:41:46 +0200

    Procfile update

--- varwish/varwish/settings.py
+++ varwish/varwish/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 REST_FRAMEWORK = {
 	# Use Django's standard `django.contrib.auth` permissions,
