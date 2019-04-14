e76d7c5536938c4c9cff6d99d5356ebce2179f12
tonyskansf <tonyngo.zf@gmail.com>
2019-04-14 20:54:29 +0200

    another damn update, hopefully the last one

--- varwish/varwish/wsgi.py
+++ varwish/varwish/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'varwish.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'varwish.varwish.settings')
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
