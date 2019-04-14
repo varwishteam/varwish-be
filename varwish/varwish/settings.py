9ed886d8ebc6f4353cb7b85c9685553154c2fe32
tonyskansf <tonyngo.zf@gmail.com>
2019-04-14 09:59:42 +0200

    update

--- varwish/varwish/settings.py
+++ varwish/varwish/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -138,3 +140,4 @@ ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = False
 
 ACCOUNT_SIGNUP_FORM_CLASS = 'varwish.forms.CustomSignupForm'
+django_heroku.settings(locals())
