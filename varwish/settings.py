450e0e528a4b27ccd6078e82a1084b00bd55eaf8
Hoang Anh Ngo <tonyngo.zf@gmail.com>
2019-04-18 17:00:30 +0200

    STATIC_ROOT path changed

--- varwish/settings.py
+++ varwish/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'varwish/../varwish/staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 REST_FRAMEWORK = {
