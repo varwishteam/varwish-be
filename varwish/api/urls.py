a079429c5bf7b1efa782bb7236a5143bfdd3b7a6
tonyskansf <tonyngo.zf@gmail.com>
2019-04-18 15:37:06 +0200

    changes

--- varwish/api/urls.py
+++ varwish/api/urls.py
@@ -1,6 +1,3 @@
-from allauth.account.views import LoginView
-from django.urls import path
-
 urlpatterns = [
-    path('api/', LoginView.as_view(), name='login'),
+    # path('api/', LoginView.as_view(), name='login'),
 ]
