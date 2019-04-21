4fcd1f3681dbd08332d990637e0b11e6ac38467b
Tomas Le <letomas@fit.cvut.cz>
2019-04-21 15:55:33 +0200

    Changed file structure

--- varwish/wishlists/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from wishlists.models import Wishlist
-
-
-# Register your models here.
-admin.site.register(Wishlist)
--- varwish/wishlists/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class WishlistsConfig(AppConfig):
-    name = 'wishlists'
