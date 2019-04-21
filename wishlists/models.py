9b1af03e65b8b70e35b08c1f270c3e2f39ca6331
Tomáš Le <letomas1998@gmail.com>
2019-04-21 16:30:08 +0200

    Modified wishlist model (#13)
    * Init wishlists
    
    * Created wishlist model
    
    * Created wishlist model
    
    * Created wishlist form and view
    
    * Init wishlists
    
    * Changed file structure
    
    * Added templates
    
    * Update
    
    * Updated project settings and urls
    
    * Modified wishlist model

--- wishlists/models.py
+++ wishlists/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models
+from django.conf import settings
 
 
 # Create your models here.
@@ -7,6 +8,7 @@ class Wishlist(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
 
     EMPTY = 'em'
     OPEN = 'op'
