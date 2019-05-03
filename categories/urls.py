5bf017688dbadc4fe435e80e3147a1961ddf6f4c
Tomas Le <letomas@fit.cvut.cz>
2019-05-03 13:26:44 +0200

    Updated url

--- categories/urls.py
+++ categories/urls.py
@@ -1,9 +1,17 @@
 from rest_framework.routers import DefaultRouter
 from rest_framework_nested import routers
+from django.urls import include, re_path
 
-from .views import CategoryViewSet
+
+from .views import CategoryViewSet, AttributeViewSet
 
 router = DefaultRouter()
 router.register(r'', CategoryViewSet, base_name='categories')
+category_router = routers.NestedDefaultRouter(router, r'', lookup='')
+category_router.register(r'attributes', AttributeViewSet, base_name='category-attributes')
 
-urlpatterns = router.urls
+# urlpatterns = router.urls
+urlpatterns = [
+    re_path(r'^', include(router.urls)),
+    re_path(r'^', include(category_router.urls)),
+]
