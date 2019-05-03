1905ba0a787f54483c258e4bb7790ff2d8de2cde
Tomas Le <letomas@fit.cvut.cz>
2019-05-03 13:26:07 +0200

    Attribute serializer and view

--- categories/serializers.py
+++ categories/serializers.py
@@ -1,17 +1,44 @@
 from rest_framework import serializers
 
-from .models import Category
+from .models import Category, Attribute
 from wishlists.serializers import ItemDetailSerializer
+
+
+class StringSerializer(serializers.StringRelatedField):
+	def to_internal_value(self, val):
+		return val
 
 
 class CategorySerializer(serializers.ModelSerializer):
 
 	class Meta:
 		model = Category
-		fields = ('id', 'name', 'items')
+		fields = ('id', 'name', 'items', 'attributes')
 
 	items = serializers.SerializerMethodField()
+	attributes = serializers.SerializerMethodField()
 
 	def get_items(self, obj):
 		items = ItemDetailSerializer(obj.category_items.all(), many=True).data
 		return items
+
+	def get_attributes(self, obj):
+		attributes = AttributeSerializer(obj.attributes.all(), many=True).data
+		return attributes
+
+
+class AttributeSerializer(serializers.ModelSerializer):
+
+	class Meta:
+		model = Attribute
+		fields = '__all__'
+
+	category = StringSerializer(many=False)
+
+	def create(self, request):
+		data = request.data
+		category = data.pop('category')
+		attribute = Attribute.objects.create(**data)
+		attribute.category = Category.objects.filter(id=category).get()
+		attribute.save()
+		return attribute
